f92f603cfc2f8b093cce555cc9461f33690d6a41
Akyl <akyl.kb@yandex.com>
2019-10-31 03:44:18 +0600

    fix progress

--- components/profile/information-tab.js
+++ components/profile/information-tab.js
@@ -5,7 +5,7 @@ const InformationTab = ({ data: response, user }) => {
     <div className="InformationTab">
       <div className="user-progress">
         <span className="text has-text-success">Вероятность получить работу мечты {user.rating}%</span>
-        <progress className="progress is-success" value="1" max={100}>{user.rating}%</progress>
+        <progress className="progress is-success" value={user.rating} max={100}>{user.rating}%</progress>
       </div>
       {data && data.counts && (
         <ul className="infographics">
